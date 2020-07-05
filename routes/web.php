5e3ad540148926abd2283ad57cff879b2aaa04a0
naufal randi <naufalrandialdiansyah@gmail.com>
2020-07-05 13:44:46 +0700

    Penambahan CRUD

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,11 @@ Route::get('/', function () {
     return view('index');
 })->name('home');
 
-Route::resource('artikel', 'ArtikelController');
 
-Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
-Route::post('/items', 'ItemController@store'); // menyimpan data
-Route::get('/items', 'ItemController@index'); // menampilkan semua
-Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id
-Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
-Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
-Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
+Route::get('/artikel/create', 'ArtikelController@create')->name('artikel.create'); // menampilkan halaman form
+Route::post('/artikel', 'ArtikelController@store')->name('artikel.store'); // menyimpan data
+Route::get('/artikel', 'ArtikelController@index')->name('artikel.index'); // menampilkan semua
+Route::get('/artikel/{id}', 'ArtikelController@show')->name('artikel.show'); // menampilkan detail item dengan id
+Route::get('/artikel/{id}/edit', 'ArtikelController@edit')->name('artikel.edit'); // menampilkan form untuk edit item
+Route::put('/artikel/{id}', 'ArtikelController@update')->name('artikel.update'); // menyimpan perubahan dari form edit
+Route::delete('/artikel/{id}', 'ArtikelController@destroy')->name('artikel.destroy'); // menghapus data dengan id
